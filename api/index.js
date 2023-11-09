eb6a8345882427052a938dc279740374f96e0c38
Hitendra <hitendra_kv@ee.iitr.ac.in>
2023-11-09 19:43:56 +0530

    created signup route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 mongoose
   .connect(process.env.MONGO)
@@ -18,4 +19,6 @@ app.listen(3000, () => {
   console.log("Listening to the PORT 3000!!!");
 });
 
+app.use(express.json());
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
