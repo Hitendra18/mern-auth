a47a2fe9765aacb9f71b631e7abb1f91e3586c75
Hitendra <hitendra_kv@ee.iitr.ac.in>
2023-11-09 17:17:56 +0530

    connected to mongodb

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import express from "express";
 const app = express();
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Conntected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
-  console.log("Listening to the PORT 3000!");
+  console.log("Listening to the PORT 3000!!!");
 });
