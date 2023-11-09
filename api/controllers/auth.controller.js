4efacf12eece60bbc4b1092dbe478b5afc753bcd
Hitendra <hitendra_kv@ee.iitr.ac.in>
2023-11-10 03:59:52 +0530

    added middleware and a function for custom errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,15 +1,17 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
 
   try {
     await newUser.save();
-    res.status(201).json({ message: "user is created successfullyyyy......" });
+    res.status(201).json({ message: "user is created successfully......" });
   } catch (error) {
-    res.status(500).json(error);
+    // next(errorHandler(519, "Custom error!!!!!!"));
+    next(error);
   }
 };
