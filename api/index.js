ac78d5fec3a7822bc25dee107c3f7c53192168fe
Hitendra <hitendra_kv@ee.iitr.ac.in>
2023-11-15 04:30:01 +0530

    created update user api route

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 mongoose
   .connect(process.env.MONGO)
@@ -20,6 +21,8 @@ app.listen(3000, () => {
 });
 
 app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 
