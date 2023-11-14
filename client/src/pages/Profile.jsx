cd809b731973d302c41657985252433822196831
Hitendra <hitendra_kv@ee.iitr.ac.in>
2023-11-14 18:56:13 +0530

    Completed Profile Page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,5 +1,48 @@
+import { useSelector } from "react-redux";
+
 export default function Profile() {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
-    <div>Profile</div>
-  )
+    <div className="p-3 max-w-lg mx-auto">
+      <div className="text-3xl m-7 font-semibold text-center">Profile</div>
+      <form className="flex flex-col gap-4">
+        <img
+          src={currentUser.profilePicture}
+          alt="profile"
+          className="w-24 h-24 self-center  rounded-full object-cover cursor-pointer mt-2"
+        />
+        <input
+          type="text"
+          name="username"
+          id="username"
+          placeholder="Username"
+          defaultValue={currentUser.username}
+          className="bg-slate-100 rounded-lg p-3"
+        />
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Email"
+          defaultValue={currentUser.email}
+          className="bg-slate-100 rounded-lg p-3"
+        />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Password"
+          className="bg-slate-100 rounded-lg p-3"
+        />
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-70 font-medium">
+          update
+        </button>
+      </form>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span className="text-red-700 cursor-pointer">Sign out</span>
+      </div>
+    </div>
+  );
 }
